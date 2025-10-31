57a8e5b6df21facda3baf88e7a0f37a3bd313827
valentina godoy <valentinagodoy0267@gmail.com>
2025-10-31 13:43:56 -0300

    bn

--- AppMenuDigital/Main.py
+++ AppMenuDigital/Main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from functools import wraps
-import pymysql
+from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
 import os
